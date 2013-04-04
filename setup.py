e401ebe1916c795e0412d886e5f1ab2b06336e49
Dimitris Verraros <vervas.g@gmail.com>
2013-04-04 11:58:02 +0200

    Bump version to 1.6.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.version_info < (2, 6):
 else:
     required = []
 
-required.append('pycclib>=1.2.2')
+required.append('pycclib>=1.3.0')
 required.append('argparse>=1.1')
 
 srcscripts = ['cctrl/cctrlapp', 'cctrl/cctrluser', 'cctrl/cctrltunnel']
